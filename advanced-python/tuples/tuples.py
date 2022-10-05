ce408b92b5eab9ee18d5613bbf40593475b12c5c
malikmoss <malik@govalo.com>
2022-10-04 23:56:18 -0500

    tuples folder

---
+++ advanced-python/tuples/tuples.py
@@ -0,0 +1,29 @@
+
+# my_tuple = tuple(["Maya",28, "Detroit"])
+# print(my_tuple)
+
+# item = my_tuple[2]
+# print(item)
+
+# for i in my_tuple:
+#     print(i)
+
+# my_tuple2 = ('a','p','p','l','e')
+# print(my_tuple2.count('a'))
+# print(my_tuple2.index('l'))
+
+# my_list = list(my_tuple2)
+# print(my_list)
+
+# list_to_tuple = tuple(my_list)
+# print(list_to_tuple)
+
+# print(my_tuple2[::3])
+
+# mytuple3 = (0,1,2,3,4)
+
+# i1, *i2, i3 = mytuple3
+
+# print(i1)
+# print(i2)
+# print(i3)
